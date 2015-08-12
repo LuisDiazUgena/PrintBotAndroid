ffc183f72663bfae1fc1d61063efbc54d5c120ea
Luis Diaz <luisdiazugena@gmail.com>
2015-08-12 15:05:25 +0200

    Added onClick to every button

--- PrintBotAndroid/app/src/main/java/com/luisdiaz/printbotandroid/MainActivity.java
+++ PrintBotAndroid/app/src/main/java/com/luisdiaz/printbotandroid/MainActivity.java
@@ -1,14 +1,15 @@
 package com.luisdiaz.printbotandroid;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,15 +33,101 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnConnect =(Button) findViewById(R.id.BtnConnect);
+        btnDisconnect=(Button) findViewById(R.id.BtnDisconnect);
+
+        btnLight = (Button) findViewById(R.id.BtnLight);
+        btnObstacles = (Button) findViewById(R.id.BtnObstacles);
+        btnLines = (Button) findViewById(R.id.BtnLines);
+
+        btnUp=(Button) findViewById(R.id.BtnUp);
+        btnLeft = (Button) findViewById(R.id.BtnLeft);
+        btnCenter = (Button) findViewById(R.id.BtnCenter);
+        btnRight = (Button) findViewById(R.id.BtnRight);
+        btnDown =(Button) findViewById(R.id.BtnDown);
+
+
         btnSend = (Button) findViewById(R.id.BtnSend);
 
         userInput = (EditText) findViewById(R.id.UserInput);
         consoleText = (TextView)findViewById(R.id.ConsoleText);
 
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Connect");
+            }
+        });
+
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Disconnect");
+            }
+        });
+
+        btnLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Light");
+            }
+        });
+
+        btnObstacles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Obstacles");
+            }
+        });
+
+        btnLines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Lines");
+            }
+        });
+
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Up");
+            }
+        });
+
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Left");
+            }
+        });
+
+        btnCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Center");
+            }
+        });
+
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Right");
+            }
+        });
+
+        btnDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Clicked on Down");
+            }
+        });
+
+
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 consoleText.setText(userInput.getText().toString() + "\n>_");
+                showToast("Clicked on Send");
             }
         });
 
@@ -69,6 +156,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void showToast(String txt){
-        
+        Context context = getApplicationContext();
+        //CharSequence text = txt;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, txt, duration);
+        toast.show();
     }
 }
